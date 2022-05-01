8c45f7eafd222515c5969226db62f26ad4cbf955
Can <canlhotse.de>
2022-05-01 16:07:45 +0200

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,59 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "CAWS",
+  tagline: "Cool Awesome Web Stuff",
+  url: "https://caws.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "d0utone/caws", // Usually your GitHub org/user name.
+  projectName: "caws",
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/d0utone/caws",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/d0utone/caws",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "CAWS",
+        logo: {
+          alt: "CAWS Logo",
+          src: "/img/logo-light.svg",
+        },
+        items: [{ to: "/blog", label: "Blog", position: "left" }],
+      },
+      footer: {
+        style: "dark",
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Cool Awesome Web Stuff, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
